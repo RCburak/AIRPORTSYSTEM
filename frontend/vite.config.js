294edd619b6c1697238e8aaade1ab9cea64e1cf8
RCburak <kaya.burak.ibrahim42@gmail.com>
2026-01-19 22:07:30 +0300

    İlk yükleme

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Bu importun olduğundan emin ol
+import tailwindcss from '@tailwindcss/vite' //
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Plugin listesinde mutlaka bulunmalı
+    tailwindcss(), //
   ],
 })
